6af5c71b2992bf115973a111e22fa9e340d6142e
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-01-04 23:07:23 -0800

    v1

---
+++ src/main/java/it/UTeam/Onlinevideoacademyserverjava/OnlineVideoAcademyServerJavaApplication.java
@@ -0,0 +1,13 @@
+package it.UTeam.Onlinevideoacademyserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoAcademyServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVideoAcademyServerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/UTeam/Onlinevideoacademyserverjava/OnlineVideoAcademyServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.UTeam.Onlinevideoacademyserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVideoAcademyServerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
